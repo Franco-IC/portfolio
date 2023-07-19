c4104074a0502b9f7236246f4d7ccd4e42d67a70
Franco-IC <francoit1999@gmail.com>
2023-07-19 15:53:41 -0300

    changed ZoomIn scale and duration

--- src/components/animations/ZoomIn.jsx
+++ src/components/animations/ZoomIn.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 function ZoomIn({ children }) {
   return (
-    <motion.div whileHover={{ scale: 0.96, transition: { duration: 0.3 } }}>
+    <motion.div whileHover={{ scale: 0.98, transition: { duration: 0.2 } }}>
       {children}
     </motion.div>
   );
